cfa8973b5a2c41dbe287dd04cac0d814dd80c94d
dxh <1137342981@qq.com>
2019-12-11 10:41:21 +0800

    first change

--- git.js
+++ git.js
@@ -1,5 +1,14 @@
+const http = require("http")
+
+
 // 第一次创建
 
+// 这是一个服务器
+let server = http.createServer((req,res)=> {
+    res.write("哈哈哈")
+    res.end("hello,world")
+})
 
- 
+// 第一次修改
+server.listen(8080);
 
